01f71c149e5b43e2e41be5083080039470baf60f
Ben Cuff <bencuff14@gmail.com>
2025-02-22 21:08:18 -0500

    fix: remove console log for API request in AskAI component

--- components/ask-ai.tsx
+++ components/ask-ai.tsx
@@ -14,7 +14,6 @@ export default function AskAIScreen({ question }: { question: Question }) {
 					"You are trying to answer the users question that they got on a coding interview. Give a brief explanation on how to solve it without actually coding anything. This is the prompt: " +
 					question.prompt;
 
-				console.log("Sending request to API...", message);
 				const response = await fetch(
 					`${process.env.EXPO_PUBLIC_BASE_URL}/openai/ask-ai`,
 					{
